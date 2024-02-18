1e1ed093f5c4868446e26550a9c9c6a13c46eb42
duonganh203 <rankill52@gmail.com>
2024-02-18 13:47:27 +0700

    adding crud for cart

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -7,5 +7,4 @@ const authRoutes: Router = Router();
 authRoutes.post("/signup", errorHandler(signup));
 authRoutes.post("/login", errorHandler(login));
 authRoutes.get("/me", [authMiddleware], errorHandler(getMe));
-//s
 export default authRoutes;
