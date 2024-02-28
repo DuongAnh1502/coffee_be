0f72049740a855e4b322b0e8c27b160ef840996a
duonganh203 <rankill52@gmail.com>
2024-02-28 22:53:32 +0700

    adding create order

--- src/controllers/order.ts
+++ src/controllers/order.ts
@@ -1,4 +1,5 @@
 import { Request, Response } from "express";
+import { prismaClient } from "..";
 // 1. to create a transaction
 // 2. to list all the cart items and proceed if cart is not empty
 // 3. calculate the total amount of the order
@@ -8,4 +9,57 @@ import { Request, Response } from "express";
 // 7. create event
 // 8. to empty the cart
 
+export const createOrder = async (req: Request, res: Response) => {
+    return await prismaClient.$transaction(async (tx) => {
+        const cart = await tx.cart.findMany({
+            where: {
+                userId: req.user.id,
+            },
+            include: {
+                product: true,
+            },
+        });
+        if (cart.length === 0) {
+            return res.json({ message: "Cart is empty" });
+        }
+        const price = cart.reduce((acc, item) => {
+            return acc + item.quantity * +item.product.price;
+        }, 0);
+        const address = await tx.address.findFirst({
+            where: {
+                id: req.user.defaultShippingAddress!,
+            },
+        });
+        const order = await tx.order.create({
+            data: {
+                userId: req.user.id,
+                totalPrice: price,
+                address: address?.formattedAddress!,
+                status: "PENDING",
+                products: {
+                    create: cart.map((item) => {
+                        return {
+                            productId: item.id,
+                            quantity: item.quantity,
+                        };
+                    }),
+                },
+            },
+        });
+        await tx.orderEvent.create({
+            data: {
+                orderId: order.id,
+            },
+        });
+        await tx.cart.deleteMany({
+            where: {
+                userId: req.user.id,
+            },
+        });
+        return res.json(order);
+    });
+};
+
 export const getListOrder = async (req: Request, res: Response) => {};
+
+export const getOrderByUser = async (req: Request, res: Response) => {};
--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -1,10 +1,5 @@
+import { User } from "@prisma/client";
 import express from "express";
-interface User {
-    id: string;
-    email: string;
-    name: string | null;
-    role: string;
-}
 declare module "express-serve-static-core" {
     export interface Request {
         user: User;
