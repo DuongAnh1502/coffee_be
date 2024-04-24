5fdaa670749640dce0748b97c5ff97aa42c7303c
duonganh203 <rankill52@gmail.com>
2024-04-24 16:11:47 +0700

    testtt

--- src/routes/address.ts
+++ src/routes/address.ts
@@ -8,6 +8,7 @@ import {
 } from "../controllers/address";
 const a = 3;
 const b = 4;
+const d = 5;
 const addressRoutes = Router();
 
 addressRoutes.post("/", [authMiddleware], errorHandler(addAddress));
