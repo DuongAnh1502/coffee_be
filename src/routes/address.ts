38cffd93bb6cd716dce23ddf04b8f163d81188d0
duonganh203 <rankill52@gmail.com>
2024-04-24 16:45:29 +0700

    test conflict

--- src/routes/address.ts
+++ src/routes/address.ts
@@ -7,8 +7,7 @@ import {
     updateAddress,
 } from "../controllers/address";
 const a = 3;
-const b = 4;
-const d = 5;
+
 const addressRoutes = Router();
 
 addressRoutes.post("/", [authMiddleware], errorHandler(addAddress));
